1f43172e4f075b694f065dc718385093cdf6eaef
Satria Sahputra <satria.unp@gmail.com>
2018-10-05 03:04:19 +0700

    Add customer and order model

---
+++ database/migrations/2018_10_04_195546_create_orders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('number');
+            $table->unsignedInteger('customer_id');
+            $table->decimal('total', 15, 2);
+            $table->decimal('sub_total', 15, 2);
+            $table->decimal('discount', 15, 2);
+            $table->decimal('tax', 15, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
